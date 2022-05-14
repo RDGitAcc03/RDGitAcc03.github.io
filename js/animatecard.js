7c13b825192ec8312ad61acca2e8720756719b4a
RD <randahn03@gmail.com>
2022-05-14 20:14:12 +0300

    finished__

--- js/animatecard.js
+++ js/animatecard.js
@@ -3,7 +3,7 @@ let cardElement = document.getElementById('card'); // Grabbing card DOM element
 
 function hinge(){
     cardElement.classList.add('animate__animated', 'animate__hinge'); // Adding classes found in node_modules > animate.css
-    // console.log(cardElement.contains('animate__animated')); // Testing if class added exists
+    console.log(cardElement.contains('animate__animated')); // Testing if class added exists
 }
 // Show animation hinge() upon clicking event of card
 cardElement.addEventListener('click', hinge);
